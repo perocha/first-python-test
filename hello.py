abe7cb147989975a281473e2191682b62919626f
procha <pedrojrocha@outlook.com>
2021-10-31 11:34:27 +0100

    Added new files with some basic Python examples

--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
 print ("hello world updated!")
 
 name = "Pedro"
-print (f"My name is {name}")
+age = "47"
+print (f"My name is {name} and my age is {age}")
+print ("My name is " + name + " and my age is " + age)
+
+
+print (name.capitalize())
+print (name.casefold())
+print (name.isalpha())
+print (name.isalnum())
+print (name.upper())
+print (name.lower())
+
+index = 0
+while index < len(name):
+    print (name[index])
+    index += 1
+
+print (name.index("e"))
+name = "Pedroooo"
+print (name.index("o"))
+
+print (name.replace ("o","a"))
