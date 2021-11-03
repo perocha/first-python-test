59f7563bf57ece6d2525c8a9fd47c3bf80202eec
procha <pedrojrocha@outlook.com>
2021-11-03 13:09:08 +0100

    Addition refactor to correct pylint errors

--- _16_use_class.py
+++ _16_use_class.py
@@ -17,5 +17,5 @@ print (company1.stock_price_alert(300))
 print (company1.stock_price_alert(350))
 
 company3 = SmallCompany ("Small", 1230, "Retail", 20)
-print (company3.smallCompanySpecific())
+print (company3.small_company_specific())
 print (company3.stock_price_alert(50))
--- useful_tools.py
+++ useful_tools.py
@@ -1,10 +1,15 @@
+'''
+Useful tools example (to be imported by another program)
+'''
 import random
 
 THIS_IS_A_TEST = "Test"
 THIS_IS_A_CONSTANT = 23
 
 def get_file_extension (filename):
+    '''Get the file extention'''
     return filename[filename.index(".") + 1:]
 
 def roll_a_dice (num):
+    '''Just roll a dice from 0 to num'''
     return random.randint (1,num)
