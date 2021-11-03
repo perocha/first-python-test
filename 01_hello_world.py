9d14c89bf86a0435249a095b060e0330a9e8ac72
procha <pedrojrocha@outlook.com>
2021-11-03 09:34:38 +0100

    Refactor hello world program with linting feedback

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,25 +1,32 @@
-print ("hello world updated!")
+'''
+My first hello world program
+'''
+def hello_world():
+    ''' Function that will print some hello world messages'''
+    print ("hello world!")
 
-my_name = "Pedro"
-my_age = "47"
-print (f"My name is {my_name} and my age is {my_age}")
-print ("My name is " + my_name + " and my age is " + my_age)
+    my_name = "Pedro"
+    my_age = "47"
+    print (f"My name is {my_name} and my age is {my_age}")
+    print ("My name is " + my_name + " and my age is " + my_age)
 
 
-print (my_name.capitalize())
-print (my_name.casefold())
-print (my_name.isalpha())
-print (my_name.isalnum())
-print (my_name.upper())
-print (my_name.lower())
+    print (my_name.capitalize())
+    print (my_name.casefold())
+    print (my_name.isalpha())
+    print (my_name.isalnum())
+    print (my_name.upper())
+    print (my_name.lower())
 
-my_index = 0
-while my_index < len(my_name):
-    print (my_name[my_index])
-    my_index += 1
+    my_index = 0
+    while my_index < len(my_name):
+        print (my_name[my_index])
+        my_index += 1
 
-print (my_name.index("e"))
-name = "Pedroooo"
-print (my_name.index("o"))
+    print (my_name.index("e"))
+    my_name = "Pedroooo"
+    print (my_name.index("o"))
 
-print (my_name.replace ("o","a"))
+    print (my_name.replace ("o","a"))
+
+hello_world ()
